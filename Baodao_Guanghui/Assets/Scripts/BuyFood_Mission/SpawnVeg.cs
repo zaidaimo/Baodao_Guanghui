a38477b4218cdbb7b2335c09ac831b59e84667b6
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2020-07-25 15:48:38 +0800

    買蔡錯誤

---
+++ Baodao_Guanghui/Assets/Scripts/BuyFood_Mission/SpawnVeg.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnVeg : MonoBehaviour
+{
+    public Transform[] SpawnPoints; //生成一個座標的陣列
+    public float spawnTime = 2f; //兩秒生成一次蔬菜
+    public GameObject Vegs;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnVegs", spawnTime, spawnTime);
+    }
+
+    void SpawnVegs()
+    {
+        int spawnIndex = Random.Range(0, SpawnPoints.Length);
+        Instantiate(Vegs, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+    }
+}
---
+++ Baodao_Guanghui/Assets/Scripts/BuyFood_Mission/Unit_Base.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Unit_Base : MonoBehaviour
+{
+    public Rigidbody2D Vegetable_rig = null;  //給蔬菜鋼體
+    //public int veg_ScoreBase = 10;  //分數的基礎值
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Box_Ctrl bc = collision.transform.parent.GetComponent<Box_Ctrl>();  //取Parent上面的腳本
+        if (collision.gameObject.name == "Box")  //如果蔬菜碰到的物件是箱子
+        {
+            
+            Destroy(gameObject);  //銷毀蔬菜物件
+            Debug.Log("銷毀蔬菜");
+        }
+    }
+   
+
+}
