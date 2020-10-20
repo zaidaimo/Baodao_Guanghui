40cb21d2ba1743892ed46f03c87de992ea55ad88
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2020-10-20 22:36:24 +0800

    20201020

---
+++ Baodao_Guanghui/Assets/Scripts/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    public GameObject Photo;
+    public GameObject Story;
+    public GameObject Achive;
+
+
+    private void OnMouseDown()
+    {
+        Photo.SetActive(true);
+        Story.SetActive(false);
+        Achive.SetActive(false);
+
+    }
+}
---
+++ Baodao_Guanghui/Assets/Scripts/Storys.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storys : MonoBehaviour
+{
+    public GameObject Photo;
+    public GameObject Story;
+    public GameObject Achive;
+
+    public GameObject Shop;
+    public GameObject Picture;
+    public GameObject Left;
+    public GameObject Right;
+
+    private void OnMouseDown()
+    {
+        Photo.SetActive(false);
+        Story.SetActive(true);
+        Achive.SetActive(false);
+
+        Achive.SetActive(false);
+
+        Achive.SetActive(false); Achive.SetActive(false);
+    }
+}
