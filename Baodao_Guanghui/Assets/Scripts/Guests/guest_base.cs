12ed3598624e4fb18d4bed87409f6a203bf0984c
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2020-11-09 16:15:40 +0800

    20201109

---
+++ Baodao_Guanghui/Assets/Scripts/Guests/guest_base.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guest_base : MonoBehaviour
+{
+    public Rigidbody2D Guest_rig = null;  //給肉鋼體
+    public float DestroyTime;
+
+    public Player script;
+    public bool isGame;  //是否正在進行遊戲
+
+    private void Start()
+    {
+        print("遊戲開始");
+        Destroy(gameObject, DestroyTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isGame == true)  //如果肉碰到的物件是箱子 && isGame == true
+        {
+
+            Destroy(gameObject);
+            Debug.Log("客人離開");
+
+        }
+    }
+}
