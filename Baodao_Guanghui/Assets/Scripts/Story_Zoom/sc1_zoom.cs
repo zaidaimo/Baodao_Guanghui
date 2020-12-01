323c8b1e5bc9a8ef5d587e949179adb33bf4ead8
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2020-12-01 23:06:11 +0800

    202012011106

--- Baodao_Guanghui/Assets/Scripts/Story_Zoom/sc1_zoom.cs
+++ Baodao_Guanghui/Assets/Scripts/Story_Zoom/sc1_zoom.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class sc1_zoom : MonoBehaviour
 {
-    Vector2 targetPosition = new Vector2(-197, 0);   // 目標位置
+    Vector2 targetPosition = new Vector2(230, 250);   // 目標位置
     Vector2 currentVelocity = Vector2.zero;     // 當前速度，這個值由你每次呼叫這個函式時被修改
-    float maxSpeed = 90f;    // 選擇允許你限制的最大速度
-    float smoothTime = 1f;      // 達到目標大約花費的時間。 一個較小的值將更快達到目標。
+    float maxSpeed = 120f;    // 選擇允許你限制的最大速度
+    float smoothTime = 0.6f;      // 達到目標大約花費的時間。 一個較小的值將更快達到目標。
 
     void Update()
     {
         transform.position = Vector2.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime, maxSpeed);
+
     }
 }
