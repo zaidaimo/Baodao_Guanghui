8b55ebd9e71747b3d773a2dadea910c8cdd5d940
zaidaimo <55180654+zaidaimo@users.noreply.github.com>
2020-12-14 22:40:13 +0800

    20201214

---
+++ Baodao_Guanghui/Assets/Scripts/Guests/Do_Hua_Guest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Do_Hua_Guest : MonoBehaviour
+{
+    public GameObject Do_Hua_Quiz;
+
+    public GameObject Right;
+    public GameObject Left;
+    public GameObject Shop;
+    public GameObject Picture;
+    private void OnMouseDown()
+    {
+        Do_Hua_Quiz.SetActive(true);
+
+        Right.SetActive(false);
+        Left.SetActive(false);
+        Shop.SetActive(false);
+        Picture.SetActive(false);
+    }
+}
